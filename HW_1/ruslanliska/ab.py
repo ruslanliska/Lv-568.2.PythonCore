ae05fca5a50a4d0558d71670964dfacbde29f822
ruslanliska <ruslan.liska@gmail.com>
2021-01-29 19:52:53 +0200

    Added HW_1 folder

---
+++ HW_1/ruslanliska/ab.py
@@ -0,0 +1,12 @@
+"""This is defining variables and outputting results."""
+
+a = 393
+b = 19
+print('a =', a)
+print('b =', b)
+print('Result (a+b):', a + b)
+print('Result (a-b):', a - b)
+print('Result (a/b):', a / b)
+print('Result (a*b):', a * b)
+print('Result (a**b):', a ** b)
+print('Result (a%b):', a % b)
